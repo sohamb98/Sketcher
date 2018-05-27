71d08d92f0a2dff7b704e7da8feffbca88c8a68d
sohamb98 <bhattacharya.soham@outlook.com>
2018-05-27 17:42:23 +0530

    Canny Edge detection Added

--- openCV/Source1.cpp
+++ openCV/Source1.cpp
@@ -10,11 +10,16 @@ using namespace cv;
 double thresh;
 double thresh_max = 225;
 int thresh_slide = 127;
+int ratio = 3;
+int lowThreshold;
+int const max_lowThreshold = 100;
 
 int main() {
 	Mat img;
 	Mat img_grey;
 	Mat threshimg;
+	Mat edge;
+	Mat draw;
 	
 	double maxValue = 255;
 	VideoCapture cam(0);
@@ -25,6 +30,7 @@ int main() {
 		imshow("camfeed", img);
 		namedWindow("greyscale", WINDOW_AUTOSIZE);
 		imshow("greyscale", img_grey);
+
 		threshold(img_grey, threshimg, thresh, maxValue, THRESH_BINARY);
 		namedWindow("threshold", WINDOW_AUTOSIZE);
 
@@ -34,6 +40,17 @@ int main() {
 		thresh = thresh_slide;
 
 		imshow("threshold", threshimg);
+
+		namedWindow("CannyEdge", WINDOW_AUTOSIZE);
+		createTrackbar("MinThreshold:", "CannyEdge", &lowThreshold, max_lowThreshold);
+
+		blur(img_grey,edge, Size(3, 3));
+
+		Canny(edge, edge, lowThreshold, lowThreshold*ratio, 3);
+		edge.convertTo(draw, CV_8U);
+		imshow("CannyEdge", draw);
+
+		
 		if (waitKey(1)==27) {
 			break;
 		}
